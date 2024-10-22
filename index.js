bd151d196d7a0b302de3a713d8f5a8d188161b73
Felipe Rondello <ferondellodev@gmail.com>
2024-10-22 17:28:01 -0300

    Exercicio finalizado

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Olá mundo")
+const prompt = require("prompt-sync")()
+
+var Nome = prompt("Nome do heroi: ")
+var xp = prompt("xp dp heroi: ")
+var elo;
+if(xp<1000){
+    elo = "Ferro";
+}
+else if(xp>=1001 && xp<=2000){
+    elo = "Bronze";
+}
+else if(xp >= 2001 && xp <= 5000){
+    elo = "Prata";
+}
+else if (xp >= 5001 && xp <= 7000){
+    elo = "Ouro";
+}
+else if(xp >= 7001 && xp <= 8000){
+    elo = "Platina";
+}
+else if (xp >= 8001 && xp <= 9000){
+    elo = "Ascendente";
+}
+else if(xp >= 9001 && xp <= 10000){
+    elo = "Imortal";
+}
+else if (xp > 10000){
+    elo = "Radiante";
+}
+
+console.log("O heroi de nome "+Nome+" esta no novel de "+ elo);
+
